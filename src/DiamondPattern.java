d040ec850b101deaef444d866c7c920196543d73
Tanvir-yzu <2020tanvir1971@gmail.com>
2024-03-17 00:45:16 +0800

    all

--- src/DiamondPattern.java
+++ src/DiamondPattern.java
@@ -3,33 +3,33 @@ public class DiamondPattern {
         int n = 5; // Change this value to adjust the size of the diamond
 
         // Upper half of the diamond
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = 0; i < n; i++) { // Loop for each row in the upper half
+            for (int j = 0; j < n - i - 1; j++) { // Loop to print spaces before the diamond
                 System.out.print(" ");
             }
-            System.out.print("<>");
-            if (i > 0) {
-                for (int j = 0; j < 2 * i - 1; j++) {
+            System.out.print("<>"); // Print the left side of the diamond
+            if (i > 0) { // For rows after the first one
+                for (int j = 0; j < 2 * i - 1; j++) { // Loop to print dots between the diamonds
                     System.out.print(".");
                 }
-                System.out.print("<>");
+                System.out.print("<>"); // Print the right side of the diamond
             }
-            System.out.println();
+            System.out.println(); // Move to the next line after printing each row
         }
 
         // Lower half of the diamond
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 0; j < n - i - 1; j++) {
+        for (int i = n - 2; i >= 0; i--) { // Loop for each row in the lower half
+            for (int j = 0; j < n - i - 1; j++) { // Loop to print spaces before the diamond
                 System.out.print(" ");
             }
-            System.out.print("<>");
-            if (i > 0) {
-                for (int j = 0; j < 2 * i - 1; j++) {
+            System.out.print("<>"); // Print the left side of the diamond
+            if (i > 0) { // For rows before the last one
+                for (int j = 0; j < 2 * i - 1; j++) { // Loop to print dots between the diamonds
                     System.out.print(".");
                 }
-                System.out.print("<>");
+                System.out.print("<>"); // Print the right side of the diamond
             }
-            System.out.println();
+            System.out.println(); // Move to the next line after printing each row
         }
     }
 }
