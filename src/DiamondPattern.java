8e4496fb2ef678389af8961634cae562d33ed017
Tanvir-yzu <2020tanvir1971@gmail.com>
2024-03-17 00:26:31 +0800

    akk

--- src/DiamondPattern.java
+++ src/DiamondPattern.java
@@ -1,11 +1,32 @@
 public class DiamondPattern {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+        int n = 5; // Change this value to adjust the size of the diamond
+
+        // Upper half of the diamond
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("  ");
             }
-            for (int k = 1; k <= 2 * i - 1; k++) {
+            System.out.print("<>");
+            if (i > 0) {
+                for (int j = 0; j < 2 * i - 1; j++) {
+                    System.out.print(".");
+                }
+                System.out.print("<>");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print("  ");
+            }
+            System.out.print("<>");
+            if (i > 0) {
+                for (int j = 0; j < 2 * i - 1; j++) {
+                    System.out.print(".");
+                }
                 System.out.print("<>");
             }
             System.out.println();
